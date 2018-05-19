35d6340473702fc1f32afb49ed729042935c6b6d
Denis Daniliuk <denis.daniliuk@gmail.com>
2018-05-19 13:08:41 +0300

    Initial commit

---
+++ src/main/java/smartBot/SmartBotApplication.java
@@ -0,0 +1,12 @@
+package smartBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartBotApplication.class, args);
+	}
+}
